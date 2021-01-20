33e3d08ac8f914a1d71c2d12eda8ff7a54665383
이지훈 <ljihoon0330@gmail.com>
2021-01-20 20:01:41 +0900

    HomeScreen 프록시를 이용해 백엔드에서 데이터 받기

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,14 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Product from "../components/Product";
-import { data } from "../data";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
 
 const HomeScreen = props => {
+  const [products, setProducts] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const { data } = await axios.get("/api/products");
+        setProducts(data);
+        setLoading(false);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className="row center">
-      {Object.keys(data.products).map(key => {
-        console.log(data.products[key]);
-        return <Product key={key} product={data.products[key]} />;
-      })}
+    <div>
+      {loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <div className="row center">
+          {Object.keys(products).map(key => {
+            return <Product key={key} product={products[key]} />;
+          })}
+        </div>
+      )}
     </div>
   );
 };
