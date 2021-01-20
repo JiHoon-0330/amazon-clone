9645a377ffd604269e5f2077843b8007cf4104ed
이지훈 <ljihoon0330@gmail.com>
2021-01-20 19:29:39 +0900

    add express

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Product from "../components/Product";
 import { data } from "../data";
 
-const HomeScreen = props => (
-  <div className="row center">
-    {Object.keys(data.products).map(key => {
-      console.log(data.products[key]);
-      return <Product key={key} product={data.products[key]} />;
-    })}
-  </div>
-);
+const HomeScreen = props => {
+  return (
+    <div className="row center">
+      {Object.keys(data.products).map(key => {
+        console.log(data.products[key]);
+        return <Product key={key} product={data.products[key]} />;
+      })}
+    </div>
+  );
+};
 
 export default HomeScreen;
